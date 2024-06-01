cfa45cc316264c019790fa23aa53415d3703d646
hanyoungeun <hanyoungeun469@gmail.com>
2024-06-01 15:08:11 +0900

    백엔드에서 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,2 +1,26 @@
 "use strict";
-console.log("hello");
+
+const id = document.querySelector("#id"),
+ psword = document.querySelector("#psword"),
+ loginbtn = document.querySelector("button");
+
+loginbtn.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    };
+    console.log(req);
+    console.log(JSON.stringify(req));
+    fetch("/login",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(req),
+    })
+        .then((res) => res.json())
+        .then(console.log);
+
+}
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,18 +1,47 @@
 "use strict"
-const home = (req, res)=>{
-    res.render("home/index");
+
+const users ={
+    id: ["han", "young", "eun"],
+    psword: ["1234","1234","123456"],
 };
-const login = (req,res) => {
-    res.render("home/login");
+
+const output = {
+    home: (req, res)=>{
+        res.render("home/index");
+    },
+    login: (req,res) => {
+        res.render("home/login");
+    },
 };
+
+const process ={
+    login: (req,res)=>{
+        const id = req.body.id,
+        psword = req.body.psword;
+
+        if(users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx] == psword){
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하셨습니다",
+        });
+    },
+};
+
 const write = (req,res) => {
     res.render("home/write");
 };
 
 module.exports ={
-    home,
-    login,
+    output,
     write,
+    process,
 };
 // { key: value} //오브젝트는 키와 value값으로 이루어짐. 위와 같이 키값만 넣어주면 자체적으로 키값을 value값으로 넣어줌
 // {
